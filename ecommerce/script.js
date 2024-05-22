590f107dbd7885796ef00e70c14054398c00300a
Sudip-khadka <ksudip0123@gmail.com>
2024-05-22 22:20:50 +0545

    completed todays assignment on autherization with login and logout feature

--- ecommerce/script.js
+++ ecommerce/script.js
@@ -6,11 +6,47 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const password = document.getElementById('password').value;
   
     // Simple authentication logic (replace with real authentication)
-    if (username === 'admin' && password === 'password') {
-      // Redirect to main page
-      window.location.href = 'main.html';
+    
+fetch('https://dummyjson.com/auth/login', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    username: username,
+    password: password,
+    expiresInMins: 30,
+    // optional, defaults to 60
+  })
+})
+.then(res => res.json())
+.then(data=>
+  {
+    if(data.token){
+      window.location.href='main.html'
+      localStorage.setItem('token',data.token)
+      console.log('Authentication successful:', data.token);
+      // Redirect to the desired page or perform additional actions
     } else {
-      alert('Invalid username or password');
+      // Authentication failed
+      console.error('Authentication failed:', data.message);
+      document.getElementById('loginForm').innerHTML+=`<p style="color:red">User Not Found</p>`
+      // Display an error message to the user
     }
+    console.log(data)
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    // Display an error message to the user
   });
+
+
+
+})
+
+  //   if (username === 'admin' && password === 'password') {
+  //     // Redirect to main page
+  //     window.location.href = 'main.html';
+  //   } else {
+  //     alert('Invalid username or password');
+  //   }
+  // });
   
